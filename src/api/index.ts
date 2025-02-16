d50023ce4f048210a25563ce3f47d02a3492009f
Yevhenii Chunachenko <yevhenii.chumachenko@norse.digital>
2025-02-16 18:28:10 +0200

    minor fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { ILessonsItem, ISchoolboysItem, IColumnsItem} from '@/types';
 
-// Local fallback values - it's for engage of errors when start
+// just for example
 const DEFAULT_URL_LOCAL = 'http://94.131.246.109:5555';
 const CLASS_KEY_LOCAL = '2';
 
-// Use process.env value if available, otherwise fallback to local variable
+// Use environment variables otherwise local values
 const DEFAULT_URL = process.env.DEFAULT_URL || DEFAULT_URL_LOCAL;
 const CLASS_KEY = process.env.CLASS_KEY || CLASS_KEY_LOCAL;
 
@@ -19,7 +20,7 @@ const schoolApi = axios.create({
 });
 
 schoolApi.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse) => {
     const statusMessages: Record<number, string> = {
       200: 'Дані отримано успішно:',
       201: 'Запис створено успішно:',
@@ -41,10 +42,8 @@ schoolApi.interceptors.response.use(
     } else if (error.request) {
       if (error.message.includes('CORS')) {
         console.error('Помилка CORS: Сервер не дозволяє доступ з цього домену.');
-        // alert('Сервер не дозволяє доступ з цієї домену (CORS). Перевірте налаштування серверу.');
       } else {
         console.error('Помилка: Сервер не відповідає.');
-        // alert('Неможливо зв’язатися з сервером. Перевірте з’єднання.');
       }
     } else {
       console.error('Помилка налаштування запиту:', error.message);
@@ -53,14 +52,16 @@ schoolApi.interceptors.response.use(
   }
 );
 
-export const fetchData = (endpoint: string) => schoolApi.get(endpoint).then(res => res.data);
+// Типизация функций
+export const fetchData = <T>(endpoint: string): Promise<T> =>
+  schoolApi.get<T>(endpoint).then((res: AxiosResponse<T>) => res.data);
 
-export const addLesson = ({ SchoolboyId, ColumnId }: { SchoolboyId: number, ColumnId: number }) =>
-  schoolApi.post('/Rate', { SchoolboyId, ColumnId, Title: 'Н' }).then(res => res.data);
+export const addLesson = ({ SchoolboyId, ColumnId }: { SchoolboyId: number, ColumnId: number }): Promise<ILessonsItem> =>
+  schoolApi.post<ILessonsItem>('/Rate', { SchoolboyId, ColumnId, Title: 'Н' }).then((res: AxiosResponse<ILessonsItem>) => res.data);
 
-export const removeLesson = ({ SchoolboyId, ColumnId }: { SchoolboyId: number, ColumnId: number }) =>
-  schoolApi.post('/UnRate', { SchoolboyId, ColumnId }).then(res => res.data);
+export const removeLesson = ({ SchoolboyId, ColumnId }: { SchoolboyId: number, ColumnId: number }): Promise<void> =>
+  schoolApi.post<void>('/UnRate', { SchoolboyId, ColumnId }).then((res: AxiosResponse<void>) => res.data);
 
-export const fetchColumns = () => fetchData('/Column');
-export const fetchSchoolboys = () => fetchData('/Schoolboy');
-export const fetchLessons = () => fetchData('/Rate');
+export const fetchColumns = (): Promise<IColumnsItem[]> => fetchData<IColumnsItem[]>('/Column');
+export const fetchSchoolboys = (): Promise<ISchoolboysItem[]> => fetchData<ISchoolboysItem[]>('/Schoolboy');
+export const fetchLessons = (): Promise<ILessonsItem[]> => fetchData<ILessonsItem[]>('/Rate');
