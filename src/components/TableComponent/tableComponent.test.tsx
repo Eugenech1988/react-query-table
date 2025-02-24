694cf6ec6e253140d3b5b98e79c14941b42b2ba7
Yevhenii Chunachenko <yevhenii.chumachenko@norse.digital>
2025-02-24 11:46:05 +0200

    ts fiz

--- src/components/TableComponent/tableComponent.test.tsx
+++ src/components/TableComponent/tableComponent.test.tsx
@@ -5,7 +5,7 @@ import { SchoolarProvider } from '@/SchoolarContext.tsx';
 import TableComponent from './index.tsx';
 import * as useDataHooks from '@/hooks/useData.ts';
 import { QueryClient, QueryClientProvider, UseMutationResult } from '@tanstack/react-query';
-import { ILessonsData, ILessonsItem } from '@/types';
+import { IColumnsItem, ILessonsData, ILessonsItem, ISchoolboysItem } from '@/types';
 
 const mockNavigate = vi.fn();
 vi.mock('react-router-dom', async () => {
@@ -52,7 +52,7 @@ const mockUseTableData = (data = mockData, isLoading = false, isError = false) =
   vi.spyOn(useDataHooks, 'useTableData').mockReturnValue({
     isLoading,
     isError,
-    data,
+    data: data.map(item => item.Items as ISchoolboysItem[] | IColumnsItem[] | ILessonsItem[]),
   });
 };
 
